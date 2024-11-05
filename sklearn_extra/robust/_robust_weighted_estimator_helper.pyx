9bbb2deaf5cdd26a7e6a5612eb31f3a6d6762bf6
Timothee Mathieu <timothee.mathieu@inria.fr>
2024-11-05 13:35:09 +0100

    try fix windows

--- sklearn_extra/robust/_robust_weighted_estimator_helper.pyx
+++ sklearn_extra/robust/_robust_weighted_estimator_helper.pyx
@@ -14,6 +14,7 @@ import sys
 from time import time
 
 from libc.math cimport exp, log, sqrt, pow, fabs
+from libc.stdint cimport int64_t
 cimport numpy as np
 from numpy.math cimport INFINITY
 
@@ -66,7 +67,7 @@ cpdef np.ndarray[floating] _kmeans_loss(np.ndarray[floating, ndim=2, mode='c'] X
         np.ndarray[floating, ndim=2] centers = np.zeros([n_classes,
                                                          n_features],
                                                          dtype = dtype)
-        np.ndarray[long] num_in_cluster = np.zeros(n_classes, dtype = int)
+        np.ndarray[int64_t] num_in_cluster = np.zeros(n_classes, dtype = int)
         np.ndarray[floating] inertias = np.zeros(n_samples, dtype = dtype)
     for i in range(n_samples):
         for j in range(n_features):
